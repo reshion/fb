e4caf44607a50d721ada7db89be092c2f64a0cce
rico <rico@Main>
2015-05-14 15:57:29 +0200

    ini

---
+++ resources/views/be/user/list.blade.php
@@ -0,0 +1,25 @@
+@extends('app')
+@section('content')
+<h1>User</h1>
+<table class="table">
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>E-Mail</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+
+        @foreach($users as $user)
+        <tr>
+            <td>{{$user->name}}</td>
+            <td>{{$user->email}}</td>
+            <td><a href="{{ url('user/edit/' . $user->id) }}"><i class="glyphicon glyphicon-edit"></i></a></td>
+        </tr>
+        @endforeach
+
+    </tbody>
+</table>
+@endsection
+
