907993de0f9f7dc9c1d9009f2f7bb29bcfe13e76
rico.mutze <rico.mutze@T4M-APC-180.trans4mation.local>
2015-06-10 14:41:35 +0200

    meta tag viewport

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
 
 	<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
 
 	<style>
