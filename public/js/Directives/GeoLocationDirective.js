99beb8f8c1589b7c1c8b17aca223800245632e6e
rico.mutze <rico.mutze@T4M-APC-180.trans4mation.local>
2015-06-11 11:55:08 +0200

    style + toggle + panel

--- public/js/Directives/GeoLocationDirective.js
+++ public/js/Directives/GeoLocationDirective.js
@@ -5,79 +5,85 @@ app.directive('geoLocation', function($window, $rootScope) {
 	templateUrl: 'templates/GeoLocation.html',
 	scope: {
 	    internCoords: '=interncoords',
-            internLoading: '=loading'
+	    internLoading: '=loading'
 	},
 	link: function(scope, element, attrs) {
-	    if ($window.navigator && $window.navigator.geolocation) {
-                // set loading true by key
-                scope.internLoading.push('geoKey');
-		$window.navigator.geolocation.getCurrentPosition(function(position) {
-                    // remove loading key
-                    scope.internLoading.indexOf('geoKey') > -1 ? scope.internLoading.splice(scope.internLoading.indexOf('geoKey')):null;
-		    var lat = position.coords.latitude;
-		    var lng = position.coords.longitude;
-		    var newLatLng = new google.maps.LatLng(lat, lng)
-		    scope.internCoords.lng = lng;
-		    scope.internCoords.lat = lat;
-		    scope.$apply();
+	    scope.refreshPosition = function() {
+		    getLocation();
+		}
+	    getLocation = function() {
+		if ($window.navigator && $window.navigator.geolocation) {
+		    // set loading true by key
+		    scope.internLoading.push('geoKey');
+		    $window.navigator.geolocation.getCurrentPosition(function(position) {
+			// remove loading key
+			scope.internLoading.indexOf('geoKey') > -1 ? scope.internLoading.splice(scope.internLoading.indexOf('geoKey')) : null;
+			var lat = position.coords.latitude;
+			var lng = position.coords.longitude;
+			var newLatLng = new google.maps.LatLng(lat, lng)
+			scope.internCoords.lng = lng;
+			scope.internCoords.lat = lat;
+			scope.$apply();
 
-		    var myLatlng = new google.maps.LatLng(lat, lng);
+			var myLatlng = new google.maps.LatLng(lat, lng);
 
-		    var mapOptions = {
-			center: myLatlng,
-			zoom: 14
-		    };
-		    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-		    var marker = new google.maps.Marker({
-			position: myLatlng,
-			map: map,
-			title: 'There you are!'
-		    });
+			var mapOptions = {
+			    center: myLatlng,
+			    zoom: 14
+			};
+			var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+			var marker = new google.maps.Marker({
+			    position: myLatlng,
+			    map: map,
+			    title: 'There you are!'
+			});
 
-		    // Watch for Controller Changes
-		    scope.$watchCollection('internCoords.lng', function(newValue, oldValue) {
-			if (newValue) {
-			    newLatLng = new google.maps.LatLng(lat, newValue)
-			    marker.setPosition(newLatLng);
-			    map.setCenter(newLatLng);
-			    setCoords(lat, newValue);
+			// Watch for Controller Changes
+			scope.$watchCollection('internCoords.lng', function(newValue, oldValue) {
+			    if (newValue) {
+				newLatLng = new google.maps.LatLng(lat, newValue)
+				marker.setPosition(newLatLng);
+				map.setCenter(newLatLng);
+				setCoords(lat, newValue);
+			    }
+			}, true)
+
+			scope.$watchCollection('internCoords.lat', function(newValue, oldValue) {
+			    if (newValue) {
+				newLatLng = new google.maps.LatLng(newValue, lng)
+				marker.setPosition(newLatLng);
+				map.setCenter(newLatLng);
+				setCoords(newValue, lng);
+			    }
+			}, true)
+
+
+			// set Position on click
+			google.maps.event.addListener(map, "mousedown", function(event) {
+			    lat = event.latLng.lat();
+			    lng = event.latLng.lng();
+			    // set Controller Scope 
+			    // Controller scope changes affects to directive scope by watchCollection
+			    setTimeout(function() {
+				scope.internCoords.lng = lng;
+				scope.internCoords.lat = lat;
+				scope.$apply();
+			    }, 1000);
+			    setCoords(lat, lng);
+			});
+
+			function setCoords(lat, lng) {
+			    element.find('#latitude').text(lat);
+			    element.find('#longitude').text(lng);
 			}
-		    }, true)
-
-		    scope.$watchCollection('internCoords.lat', function(newValue, oldValue) {
-			if (newValue) {
-			    newLatLng = new google.maps.LatLng(newValue, lng)
-			    marker.setPosition(newLatLng);
-			    map.setCenter(newLatLng);
-			    setCoords(newValue, lng);
-			}
-		    }, true)
-
-
-		    // set Position on click
-		    google.maps.event.addListener(map, "mousedown", function(event) {
-			lat = event.latLng.lat();
-			lng = event.latLng.lng();
-			// set Controller Scope 
-			// Controller scope changes affects to directive scope by watchCollection
-			setTimeout(function() {
-			    scope.internCoords.lng = lng;
-			    scope.internCoords.lat = lat;
-			    scope.$apply();
-			}, 1000);
 			setCoords(lat, lng);
+		    }, function(error) {
+			element.text("Your geolocation is not available");
 		    });
 
-		    function setCoords(lat, lng) {
-			element.find('#latitude').text(lat);
-			element.find('#longitude').text(lng);
-		    }
-		    setCoords(lat, lng);
-		}, function(error) {
-		    element.text("Your geolocation is not available");
-		});
+		}
 	    }
-
+	    getLocation();
 	}
     }
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,12 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
-   	<div class="panel panel-default col-lg-6 col-xs-12">
-	    <div class="panel-heading" data-target="#panel-1" data-toggle="collapse" aria-expanded="false">Position</div>
-	    <div id="panel-1" class="panel-body collapse in">
+   	
 		<geo-location id="geolocation" data-loading="loading" data-interncoords="coords"></geo-location>
-	    </div>
-	</div>
-	<div class="panel panel-default col-lg-6 col-xs-12">
-	    <div class="panel-heading" data-target="#panel-2" data-toggle="collapse" aria-expanded="false">Weather</div>
-	    <div id="panel-2" class="panel-body collapse in">
+	
+	
 		<city-weather data-interncoords="coords" data-loading="loading"></city-weather>
-	    </div>
-	</div>
+	
 	
     </div>
 </div>
