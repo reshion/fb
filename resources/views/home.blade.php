1298fa399a5f53e5de3ae530b549766e594c76c8
rico.mutze <rico.mutze@T4M-APC-180.trans4mation.local>
2015-06-11 10:29:11 +0200

    style + toggle + panel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,19 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
-        <h1>Home</h1>
-        <button data-toggle-for-id="geolocation"  class="btn">Position</button>
-        <geo-location id="geolocation" data-loading="loading" data-interncoords="coords"></geo-location>
-        <div class="well">
-            <city-weather data-interncoords="coords" data-loading="loading"></city-weather>
-        </div>
+   	<div class="panel panel-default col-lg-6 col-xs-12">
+	    <div class="panel-heading" data-target="#panel-1" data-toggle="collapse" aria-expanded="false">Position</div>
+	    <div id="panel-1" class="panel-body collapse in">
+		<geo-location id="geolocation" data-loading="loading" data-interncoords="coords"></geo-location>
+	    </div>
+	</div>
+	<div class="panel panel-default col-lg-6 col-xs-12">
+	    <div class="panel-heading" data-target="#panel-2" data-toggle="collapse" aria-expanded="false">Weather</div>
+	    <div id="panel-2" class="panel-body collapse in">
+		<city-weather data-interncoords="coords" data-loading="loading"></city-weather>
+	    </div>
+	</div>
+	
     </div>
 </div>
 @endsection
